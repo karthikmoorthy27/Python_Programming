de777a7e734a2871f5ba46ba1d5f9d86f8bab728
karthikmoorthy27 <karthikmoorthy27@gmail.com>
2025-12-12 21:25:33 +0530

    remove file

--- Shallow_deep_copy.py
+++
@@ -1,9 +0,0 @@
-import copy
-l1 = [1,2.5,[10,20,30],'Python']
-
-#shallow copy
-l2 = copy.copy(l1)
-print(l2)
-print(id(l1))
-print(id(l2))
-
--- ternary_operator.py
+++
@@ -1,3 +0,0 @@
-num = int(input("enter a number"))
-
-print("even") if num % 2 == 0 else print("odd")
